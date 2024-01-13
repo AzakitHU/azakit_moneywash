4fe5d51caec9e3663d182d9c2ccac555e17acbc3
Azakit <155274165+AzakitHU@users.noreply.github.com>
2024-01-13 23:09:06 +0100

    Update framework.lua

--- server/framework.lua
+++ server/framework.lua
@@ -1,5 +1,4 @@
-ESX = nil
-TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
+ESX = exports["es_extended"]:getSharedObject()
 
 function PoliceCount()
     local players = ESX.GetPlayers()
